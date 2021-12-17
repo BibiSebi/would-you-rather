40cc39a76764cf9a87b2ac6ee715b289d56d9582
Bibiana.Sebestianova <17590227+Sebi2909@users.noreply.github.com>
2021-12-17 18:12:51 +0100

    feat: templates for leader card and create - non functional templates for views

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 const Create = () => {
-  return <div>Create works!</div>;
+  return (
+    <div className="w-full flex items-center flex-col pt-16">
+      <div className="flex flex-col items-center w-1/2 mb-16">
+        <h1 className="text-5xl text-gray-500 pb-2 font-thin">
+          Would you rather....
+        </h1>
+        <span className="text-gray-500 px-24 text-center">
+          Create a new poll by enterings your questions. Gather points for each
+          poll.
+        </span>
+      </div>
+
+      <form className="w-1/2 flex flex-col">
+        <div className="flex flex-col my-2">
+          <label className="text-gray-500 mb-1">Option one</label>
+          <input
+            className="border text-gray-500 border-gray-500 rounded-lg h-12 px-4 "
+            type="text"
+          />
+        </div>
+        <div className="flex flex-col my-2">
+          <label className="text-gray-500 mb-1">Option two</label>
+          <input
+            className="border text-gray-500 border-gray-500 rounded-lg h-12 px-4"
+            type="text"
+          />
+        </div>
+      </form>
+      <button className="px-8 py-1 rounded-xl text-white bg-gray-500 mt-16">
+        Create
+      </button>
+    </div>
+  );
 };
 
 export default Create;
--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const LeaderBoard = () => {
-  return <div>LeaderBoard works!</div>;
+  const [sortedUsers, setSortedUsers] = useState([]);
+  const users = useSelector((state) => state.users);
+
+  useEffect(() => {
+    const usersArr = Object.keys(users).map((user) => users[user]);
+    if (usersArr) {
+      const sorted = usersArr.sort(
+        (a, b) =>
+          Object.keys(b.answers).length +
+          b.questions.length -
+          (Object.keys(a.answers).length + a.questions.length)
+      );
+      setSortedUsers(sorted);
+    }
+  }, [users]);
+  return (
+    <div className="w-full items-center flex flex-col pt-8">
+      <h1 className="text-5xl text-gray-500 pb-2 font-thin">Leaderboard</h1>
+      <div className="w-1/2">
+        {sortedUsers.map((user) => (
+          <div className="border w-full rounded-lg text-gray-500  items-center border-gray-500 py-4 px-8 my-4 flex">
+            <img className="rounded-full h-16 w-16" src={user.avatarURL} />
+            <div className="flex-grow flex flex-col px-4">
+              <span className="text-4xl font-thin mb-4">{user.name}</span>
+              <div className="flex font-thin">
+                <span className="mr-4">Created: {user.questions.length}</span>
+                <span>Answered: {Object.keys(user.answers).length}</span>
+              </div>
+            </div>
+
+            <div className="flex font-thin items-end">
+              <span className="text-6xl ">
+                {user.questions.length + Object.keys(user.answers).length}
+              </span>
+              <span>pts</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeaderBoard;
