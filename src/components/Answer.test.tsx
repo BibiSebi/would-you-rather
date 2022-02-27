b9522066ecb9161da621aad3030fd103087d77a5
Bibiana.Sebestianova <17590227+Sebi2909@users.noreply.github.com>
2022-02-27 15:02:54 +0100

    build: upgrading to CRA 5 - updated dependencies for CRA - updated tailwind - added testing dependencies - added mock test case

---
+++ src/components/Answer.test.tsx
@@ -0,0 +1,8 @@
+import { cleanup } from "@testing-library/react";
+afterEach(cleanup);
+
+describe("first", () => {
+  it("should ", () => {
+    // render(<Answer answer={null} handleClick={() => null} result={null} />);
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    // Example content paths...
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
   theme: {
     extend: {},
   },
